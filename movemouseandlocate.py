0756c59b1098c8fb47fac91272102cc91eea4661
StandardV <duongvuong586@gmail.com>
2023-07-04 19:03:06 -0500

    add set minimized window

--- movemouseandlocate.py
+++ movemouseandlocate.py
@@ -1,4 +1,4 @@
-import win32gui
+import win32gui,win32con
 import pyautogui
 import time
 
@@ -8,9 +8,13 @@ def toggle_application_focus(window_title):
     hwnd = win32gui.FindWindowEx(0,0,0, window_title)
     win32gui.SetForegroundWindow(hwnd)
 
-
+def get_fore_ground():
+    time.sleep(2)
+    Minimize = win32gui.GetForegroundWindow()
+    win32gui.ShowWindow(Minimize, win32con.SW_MINIMIZE)
 
 window_title = "testname"
 pyautogui.press("alt")
 time.sleep(.1)
 toggle_application_focus(window_title)
+get_fore_ground()
